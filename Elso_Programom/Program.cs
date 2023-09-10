2b6ee8a0ef725cb24a29a148d9a9db92c840ca4b
md_patrik <xd2.0.hun@gmail.com>
2023-09-10 18:56:06 +0200

    Add files via upload

---
+++ Elso_Programom/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elso_Programom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Szam_1;
+            int Szam_2;
+            int Sum = 0;
+            Console.Write("Kérem az első számot: ");
+            Szam_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Az első szám értéke: {Szam_1}");
+            Console.Write("Kérem a második számot: ");
+            Szam_2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"A második szám értéke: {Szam_2}");
+            Sum = Szam_1 + Szam_2;
+            Console.WriteLine($"A két szám összege: {Sum}");
+            Console.ReadKey();
+        }
+    }
+}
