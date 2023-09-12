cb453d94481bb28a2926839f77b48d12612f1816
md_patrik <xd2.0.hun@gmail.com>
2023-09-12 19:59:23 +0200

    Add files via upload

---
+++ Haramdik_Programom/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haramdik_Programom
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string keresztnev = "";
+            string vezeteknev = "";
+            string monogram = "";
+
+            Console.Write("Kérem a Vezetéknevét: ");
+            vezeteknev = Console.ReadLine();
+            Console.Write("Kérem a Keresztnevét: ");
+            keresztnev = Console.ReadLine();
+
+            monogram = vezeteknev[0].ToString();
+            monogram += keresztnev[0].ToString();
+            Console.Write($"A monogram: {monogram}");
+            Console.ReadKey();
+        }
+    }
+}
