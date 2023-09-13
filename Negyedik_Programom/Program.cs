9d7d9b92f4f8d396c846ef8d3c336e9d305d322a
md_patrik <xd2.0.hun@gmail.com>
2023-09-13 21:52:52 +0200

    Add files via upload

---
+++ Negyedik_Programom/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negyedik_Programom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Változó deklarálása
+            int ora = 0;
+            //Bekérés
+            Console.Write("Kérek egy óra számot: ");
+            //ora változó feltöltése a bekért értékkel
+            ora = int.Parse(Console.ReadLine());
+            //VIzsgálat 1 -> Reggeli időszak (0-8)
+            if (ora >= 0 && ora <= 8)
+            {
+                Console.WriteLine("Jó reggelt!");
+            }
+            //Vizsgálat 2 -> Nap közbeni időszak (8-18)
+            else if (ora  > 8 && ora <= 18)
+            {
+                Console.WriteLine("Szép napot!");
+            }
+            //Vizsgálat 3 -> Esti időszak (18-24)
+            else if (ora > 18 && ora <= 24)
+            {
+                Console.WriteLine("Jó éjszakát!");
+            }
+            //Hiba üzenet
+            else
+            {
+                Console.WriteLine("Sajnálom, nincs ilyen óra!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
