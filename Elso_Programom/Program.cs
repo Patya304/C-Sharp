c6a4b601de1079012437ee740ee49c3f947a7da6
md_patrik <xd2.0.hun@gmail.com>
2023-09-13 22:00:24 +0200

    Add files via upload

--- Elso_Programom/Program.cs
+++ Elso_Programom/Program.cs
@@ -10,16 +10,19 @@ namespace Elso_Programom
     {
         static void Main(string[] args)
         {
-            int Szam_1;
-            int Szam_2;
+            int Szam_1; //adat 1
+            int Szam_2; //adat 2
             int Sum = 0;
+            //Bekérés
             Console.Write("Kérem az első számot: ");
-            Szam_1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Az első szám értéke: {Szam_1}");
+            Szam_1 = Convert.ToInt32(Console.ReadLine()); //a bekért adat integer adattípussá való átalakítása
+            Console.WriteLine($"Az első szám értéke: {Szam_1}"); //bekért szám kiírása (ellenörzés képpen)
+            //Bekérés 2
             Console.Write("Kérem a második számot: ");
             Szam_2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"A második szám értéke: {Szam_2}");
-            Sum = Szam_1 + Szam_2;
+
+            Sum = Szam_1 + Szam_2; //a két bekért szám összeadása
             Console.WriteLine($"A két szám összege: {Sum}");
             Console.ReadKey();
         }
