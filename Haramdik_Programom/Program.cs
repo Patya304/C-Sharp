f3b9e1a9871651c811eb1dfb67d7bad9f56386b9
md_patrik <xd2.0.hun@gmail.com>
2023-09-13 22:04:44 +0200

    Update Program.cs

--- Haramdik_Programom/Program.cs
+++ Haramdik_Programom/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,17 +10,18 @@ namespace Haramdik_Programom
     {
         static void Main()
         {
-            string keresztnev = "";
-            string vezeteknev = "";
-            string monogram = "";
+            string keresztnev = ""; //üres string változó
+            string vezeteknev = ""; //üres string változó
+            string monogram = ""; //üres string változó
 
-            Console.Write("Kérem a Vezetéknevét: ");
+            //Bekérés
+            Console.Write("Kérem a Vezetéknevét: "); 
             vezeteknev = Console.ReadLine();
             Console.Write("Kérem a Keresztnevét: ");
             keresztnev = Console.ReadLine();
-
+            //adattípus átalakítása
             monogram = vezeteknev[0].ToString();
-            monogram += keresztnev[0].ToString();
+            monogram += keresztnev[0].ToString(); // += -> ezzel adom hozzá, és így nem lesz felül írva a "monogram" változó
             Console.Write($"A monogram: {monogram}");
             Console.ReadKey();
         }
