7f457b26a11481d4bf7a47b9462073bc563c6bdb
md_patrik <xd2.0.hun@gmail.com>
2024-09-11 12:31:16 +0200

    Add files via upload

---
+++ 20240911matematika_muveletek/20240911matematika_muveletek/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20240911matematika_muveletek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1. Feladat
+            Console.WriteLine("\t1. Feladat");
+            int oldalh;
+            int n_kerulet;
+            int n_terulet;
+            Console.Write("Add meg a négyzet oldal hosszát: ");
+            oldalh = Convert.ToInt32(Console.ReadLine());
+
+
+            n_kerulet = 4 * oldalh;
+            Console.WriteLine($"A négyzet kerülete: {n_kerulet}");
+
+            n_terulet = oldalh * oldalh;
+            Console.WriteLine($"A négyzet területe: {n_terulet}");
+
+            //2. Feladat
+            Console.WriteLine("\n\t2. Feladat");
+            int fhossz;
+            int ahossz;
+            int trm;
+            int tr_terulet;
+
+            Console.Write("Add meg a trapéz felső hosszát: ");
+            fhossz = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Add meg a trapéz alsó hosszát: ");
+            ahossz = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Add meg a trapéz magasságát: ");
+            trm = Convert.ToInt32(Console.ReadLine());
+
+            tr_terulet = ((ahossz + fhossz) * trm) / 2;
+            Console.WriteLine($"A trapéz területe: {tr_terulet}");
+
+            //3. Feladat
+            Console.WriteLine("\n\t3. Feladat");
+            int thossz;
+            int szelesseg;
+            int tg_terulet;
+            int tg_kerulet;
+
+            Console.Write("Add meg a téglalap hosszúságát: ");
+            thossz = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Add meg a téglalap szélességét: ");
+            szelesseg = Convert.ToInt32(Console.ReadLine());
+
+            tg_terulet = thossz * szelesseg;
+            Console.WriteLine($"A téglalap területe: {tg_terulet}");
+
+            tg_kerulet = 2 * (thossz + szelesseg);
+            Console.WriteLine($"A téglalap kerülete: {tg_terulet}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
