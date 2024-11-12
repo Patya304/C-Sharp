23e9e28c321c1f567f4fa753ad13b3ee0cfa22d1
md_patrik <xd2.0.hun@gmail.com>
2024-11-13 00:02:06 +0100

    Add files via upload

---
+++ Patrick/Patrick/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class ProverbsProgram
+{
+    static List<string> englishProverbs = new List<string>();
+    static List<string> hungarianProverbs = new List<string>();
+
+    static void Main()
+    {
+        // 1. Feladat: Fájlok beolvasása
+        ReadFiles();
+
+        // 2. Feladat: Angol közmondások számának kiírása
+        Console.WriteLine("2. feladat: Az angol közmondások száma: " + englishProverbs.Count);
+
+        // 3. Feladat: "all" szót tartalmazó közmondások kiírása
+        var allProverbs = englishProverbs.Where(p => p.Contains("all")).ToList();
+        Console.WriteLine("\n3. feladat: 'all' szót tartalmazó közmondások száma: " + allProverbs.Count);
+
+        // 4. Feladat: 5 véletlenszerű angol közmondás kiválasztása
+        Random rand = new Random();
+        var randomProverbs = englishProverbs.OrderBy(x => rand.Next()).Take(5).ToList();
+        Console.WriteLine("\n4. feladat: Véletlenszerűen kiválasztott közmondások:");
+        foreach (var proverb in randomProverbs)
+        {
+            Console.WriteLine(proverb);
+        }
+
+        // 5. feladat: Véletlen közmondások elemzése
+        Console.WriteLine("\n\t5. feladat:");
+
+        // 5.a: Véletlen közmondások megjelenítése
+        Console.Write("a) \n");
+        foreach (var proverb in randomProverbs)
+        {
+            Console.WriteLine(proverb);
+        }
+
+        // 5.b: Leghosszabb közmondás és hossza
+        var longestProverb = randomProverbs.OrderByDescending(p => p.Length).First();
+        Console.WriteLine("\nb) A leghosszabb közmondás: " + longestProverb + " (" + longestProverb.Length + " karakter)");
+
+        // 5.c: "t" betűk száma
+        int tCount = randomProverbs.Sum(p => p.Count(c => c == 't' || c == 'T'));
+        Console.WriteLine("\nc) 't' betűk száma a véletlen közmondásokban: " + tCount);
+
+        // 5.d: Magánhangzók száma
+        int vowelCount = randomProverbs.Sum(p => p.Count(c => "aeiouAEIOU".Contains(c)));
+        Console.WriteLine("\nd) Magánhangzók száma a véletlen közmondásokban: " + vowelCount);
+
+        // 5.g: Véletlen közmondások ábécé sorrendbe rendezése
+        randomProverbs.Sort();
+        Console.WriteLine("\ng) ABC sorrendben a véletlen közmondások:");
+        foreach (var proverb in randomProverbs)
+        {
+            Console.WriteLine(proverb);
+        }
+
+        // 5.h: Különböző szavak száma
+        var uniqueWords = new HashSet<string>(randomProverbs.SelectMany(p => p.Split(' ')));
+        Console.WriteLine("\nh) Különböző szavak száma a véletlen közmondásokban: " + uniqueWords.Count);
+
+        // 5.i: Magyar megfelelő megjelenítése
+        Console.WriteLine("\ni) A közmondások magyar megfelelői:");
+        foreach (var proverb in randomProverbs)
+        {
+            int index = englishProverbs.IndexOf(proverb);
+            if (index >= 0 && index < hungarianProverbs.Count)
+            {
+                Console.WriteLine(proverb + " - " + hungarianProverbs[index]);
+            }
+        }
+
+        // 5.j: Magánhangzók száma a magyar közmondásokban
+        int hungarianVowelCount = hungarianProverbs.Sum(p => p.Count(c => "aáeéiíoóöőuúüű".Contains(c)));
+        Console.WriteLine("\nj) A magyar közmondásokban található magánhangzók száma: " + hungarianVowelCount);
+    }
+
+    static void ReadFiles()
+    {
+        englishProverbs = File.ReadAllLines("proverbs.txt").ToList();
+        hungarianProverbs = File.ReadAllLines("kozmondasok.txt").ToList();
+    }
+}
---
+++ Patrick/Patrick/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Patrick")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Patrick")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b660c0d6-087b-4822-82dd-b7ef2d377c4f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
